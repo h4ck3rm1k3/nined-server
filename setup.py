4b7724746c8e6d68f0b13015033555bbbdf77270
James Michael DuPont <JamesMikeDuPont@gmail.com>
2018-02-01 06:34:39 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='nined-server',
+      version='0.0.1',
+      description='Nined is not E.D.',
+      author='James Michael DuPont',
+      author_email='jamesmikedupont+nined@gmail.com',
+      url='https://www.python.org/community/sigs/current/distutils-sig',
+      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+      )
