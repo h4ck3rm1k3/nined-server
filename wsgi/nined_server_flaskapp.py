de2c9e978cb07874144d8eb77a17816528e368f7
james mike dupont <jamesmikedupont@gmail.com>
2018-02-01 07:31:22 -0500

    hostname

--- wsgi/nined_server_flaskapp.py
+++ wsgi/nined_server_flaskapp.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import os
+
+
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
+listen = os.environ['NINED_SERVER_SERVICE_HOST'] # = 172.30.249.186
+port = '8080'
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
 #if __name__ == "__main__":
-app.run()
+app.run(host=listen, port = port)
