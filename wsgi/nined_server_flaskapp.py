394b18b45d23be82839ffc1fb4de8d8d8d79a21e
james mike dupont <jamesmikedupont@gmail.com>
2018-02-01 07:23:58 -0500

    more debug

--- wsgi/nined_server_flaskapp.py
+++ wsgi/nined_server_flaskapp.py
@@ -1,10 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
-
+app.config['PROPAGATE_EXCEPTIONS'] = True
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+app.run()
